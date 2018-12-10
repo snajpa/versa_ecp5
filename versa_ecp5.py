d83a443d2f9a2b98c699c4b6e5ecf2021082ea62
Florent Kermarrec <florent@enjoy-digital.fr>
2018-12-10 09:01:57 +0100

    versa_ecp5: simplify CRG, get basic soc working

--- versa_ecp5.py
+++ versa_ecp5.py
@@ -30,52 +30,28 @@ class _CRG(Module):
 
         # pll
         self.submodules.pll = pll = ECP5PLL()
-        self.comb += pll.reset.eq(~rst_n)
         pll.register_clkin(clk100, 100e6)
         pll.create_clkout(self.cd_sys, 50e6)
-        # FIXME: AsyncResetSynchronizer needs FD1S3BX support.
-        #self.specials += AsyncResetSynchronizer(self.cd_sys, rst)
-        self.comb += self.cd_sys.rst.eq(~rst_n)
-
-        # sys_clk phase shifted (for sdram)
-        sdram_ps_clk = self.cd_sys.clk
-        # FIXME: phase shift with luts, needs PLL support.
-        sdram_ps_luts = 5
-        for i in range(sdram_ps_luts):
-            new_sdram_ps_clk = Signal()
-            self.specials += Instance("LUT4",
-                p_INIT=2,
-                i_A=sdram_ps_clk,
-                i_B=0,
-                i_C=0,
-                i_D=0,
-                o_Z=new_sdram_ps_clk)
-            sdram_ps_clk = new_sdram_ps_clk
-        self.comb += self.cd_sys_ps.clk.eq(sdram_ps_clk)
-        sdram_clock = platform.request("sdram_clock")
-        self.comb += sdram_clock.eq(sdram_ps_clk)
+        self.specials += AsyncResetSynchronizer(self.cd_sys, ~pll.locked | ~rst_n)
 
 
 class BaseSoC(SoCSDRAM):
     def __init__(self, **kwargs):
-        platform = versa_ecp5.Platform(toolchain="trellis")
+        platform = versa_ecp5.Platform(toolchain="diamond")
         platform.add_extension(versa_ecp5._ecp5_soc_hat_io)
         sys_clk_freq = int(50e6)
         SoCSDRAM.__init__(self, platform, clk_freq=sys_clk_freq,
-                          l2_size=32,
+                          cpu_type="picorv32", l2_size=32,
                           integrated_rom_size=0x8000,
+                          integrated_main_ram_size=0x8000,
                           **kwargs)
 
         self.submodules.crg = _CRG(platform)
 
-        if not self.integrated_main_ram_size:
-            self.submodules.sdrphy = GENSDRPHY(platform.request("sdram"))
-            sdram_module = AS4C32M16(sys_clk_freq, "1:1")
-            self.register_sdram(self.sdrphy,
-                                sdram_module.geom_settings,
-                                sdram_module.timing_settings,
-                                controller_settings=ControllerSettings(
-                                    with_refresh=False)) # FIXME
+        # led blinking
+        led_counter = Signal(32)
+        self.sync += led_counter.eq(led_counter + 1)
+        self.comb += platform.request("user_led", 0).eq(led_counter[26])
 
 
 def main():
@@ -86,7 +62,7 @@ def main():
 
     soc = BaseSoC(**soc_sdram_argdict(args))
     builder = Builder(soc, **builder_argdict(args))
-    builder.build()
+    builder.build(toolchain_path="/usr/local/diamond/3.10_x64/bin/lin64")
 
 
 if __name__ == "__main__":
