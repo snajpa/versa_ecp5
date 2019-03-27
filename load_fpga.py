c27b3e035b390eb463610c6f8e88d5ad36c59b46
Florent Kermarrec <florent@enjoy-digital.fr>
2019-03-27 09:30:23 +0100

    load_fpga: remove use of bit_to_svf script since Trellis now generates the .svf

--- load_fpga.py
+++ load_fpga.py
@@ -2,5 +2,4 @@
 
 import os
 
-os.system("python3 openocd/bit_to_svf.py build/gateware/top.bit top.svf")
-os.system("openocd -f openocd/ecp5-versa5g.cfg -c \"transport select jtag; init; svf top.svf; exit\"")
+os.system("openocd -f openocd/ecp5-versa5g.cfg -c \"transport select jtag; init; svf build/gateware/top.svf; exit\"")
